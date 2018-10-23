f1ac8827c521d5015fa2e2c18f4343253caa6736
Vaibhav Sahu <vaibh3jec@gmail.com>
2018-10-22 20:55:02 -0600

    added for in, for of syntax

--- ConstructorFunction.js
+++ ConstructorFunction.js
@@ -5,5 +5,17 @@ function Circle(radius){
     }
 }
 
+
+
+//for in syntax
+for(let key in circle){
+    console.log(key, circle[key]);
+}
+
+//for of syntax
+for(let key of Object.keys(circle)){
+    console.log(key);
+}
+
 const circle = new Circle(10);
 console.log(circle.area());
